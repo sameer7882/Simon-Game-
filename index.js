70e311fa41ec1f33aab90cc50b3bf7c387553179
ku282 <114866452+ku282@users.noreply.github.com>
2022-10-03 15:51:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,24 @@ document.querySelector("#green").addEventListener("click", function(){
     audio.play();
 })
 
+import React from "react";
+import Linkcard from "./Linkcard";
+import "./LinkPage.css";
+const LinkPage = () => {
+  return (
+    <div className="Link-content">
+      <div className="link-top-nav">
+        <button className="addlink">Add Link</button>
+      </div>
+      <div className="link-body">
+        <Linkcard />
+      </div>
+    </div>
+  );
+};
+
+export default LinkPage;
+
 function randomColorGenerator(){
 
     var num = Math.round(Math.random() * 4);
